5d58720427bc178be7ff72d6b7c7557946a9c53a
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-09-04 17:00:43 +0530

    Add files via upload

---
+++ WEB_API_Practice/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Web_API_Practice.DTO;
+using Web_API_Practice.Models;
+
+namespace Web_API_Practice.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GellAllProducts();
+
+        Task Addproduct(Product Product);
+
+        Task<bool> DeleteProduct(int id);
+    }
+}
---
+++ WEB_API_Practice/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Web_API_Practice.DTO;
+
+namespace Web_API_Practice.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<AllProductsDTO>> GetAllProducts();
+
+        Task AddProduct(AddProduct product);
+
+        Task<bool> DeleteProductService(int id);
+    }
+}
