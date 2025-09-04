05dcd969b1f468ea97a78e85de4eba3320eb484c
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-09-04 17:01:12 +0530

    Add files via upload

---
+++ WEB_API_Practice/Repositories/ProductRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_API_Practice.Data;
+using Web_API_Practice.DTO;
+using Web_API_Practice.Interfaces;
+using Web_API_Practice.Models;
+
+namespace Web_API_Practice.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly NorthWindContext _dbContext;
+
+        public ProductRepository(NorthWindContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Product>> GellAllProducts()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
+
+
+        public async Task Addproduct(Product Product)
+        {
+            _dbContext.Products.Add(Product);
+
+            await _dbContext.SaveChangesAsync();
+        }
+        public async Task<bool> DeleteProduct(int id)
+        {
+            try
+            {
+                var result = await _dbContext.Products.FindAsync(id);
+
+                if (result == null) return false;
+
+                _dbContext.Products.Remove(result);
+                await _dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WEB_API_Practice/Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using Web_API_Practice.DTO;
+using Web_API_Practice.Interfaces;
+using Web_API_Practice.Models;
+
+namespace Web_API_Practice.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repositories;
+
+        public ProductService(IProductRepository repositories)
+        {
+            _repositories = repositories;
+        }
+
+        public async Task<IEnumerable<AllProductsDTO>> GetAllProducts()
+        {
+            var Products = await _repositories.GellAllProducts();
+
+            return Products.Select(s => new AllProductsDTO
+            {
+                ProductId = s.ProductId,
+                ProductName = s.ProductName,
+                UnitPrize = (decimal)s.UnitPrice
+            });
+        }
+
+
+        public async Task AddProduct(AddProduct product)
+        {
+            var newProduct = new Product
+            {
+                ProductName = product.ProductName,
+                UnitPrice = product.UnitPrize,
+                CategoryId = product.CategoryID,
+                SupplierId = product.ShipperID
+            };
+
+            await _repositories.Addproduct(newProduct);
+        }
+
+        public async Task<bool> DeleteProductService(int id)
+        {
+            return await _repositories.DeleteProduct(id);
+        }
+    }
+}
