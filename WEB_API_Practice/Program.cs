88f36719c345ac2c7939c2efe60dc7b7602cb177
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-09-04 17:01:39 +0530

    Add files via upload

---
+++ WEB_API_Practice/Program.cs
@@ -0,0 +1,60 @@
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using Web_API_Practice.Data;
+using Web_API_Practice.Interfaces;
+using Web_API_Practice.Repositories;
+using Web_API_Practice.Services;
+
+namespace Web_API_Practice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+
+            // Register repository
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Register service
+            builder.Services.AddScoped<IProductService, ProductService>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Register DbContext with connection string
+            builder.Services.AddDbContext<NorthWindContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
+
+
+
