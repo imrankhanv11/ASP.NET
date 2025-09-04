e10c7d8bc1d853cf1e1ed0d9a14681313773ba05
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-09-04 16:59:54 +0530

    Add files via upload

---
+++ WEB_API_Practice/DTO/AddProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_API_Practice.DTO
+{
+    public class AddProduct
+    {
+        public string ProductName { get; set; }
+        public decimal UnitPrize { get; set; }
+
+        public int CategoryID { get; set; }
+        public int ShipperID { get; set; }
+    }
+}
---
+++ WEB_API_Practice/DTO/AllProductsDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_API_Practice.DTO
+{
+    public class AllProductsDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrize { get; set; }
+    }
+}
