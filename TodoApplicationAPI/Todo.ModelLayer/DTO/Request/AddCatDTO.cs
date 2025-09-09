58cb88d1dbedd771ec61847b046ba986f0040a4b
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-09-09 16:42:38 +0530

    Add files via upload

--- TodoApplicationAPI/Todo.ModelLayer/DTO/Request/AddCatDTO.cs
+++ TodoApplicationAPI/Todo.ModelLayer/DTO/Request/AddCatDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Todo.ModelLayer.DTO.Request
 {
     public class AddCatDTO
     {
+        [Required]
+        [StringLength(50, MinimumLength =3, ErrorMessage ="Minimum More than 3 char")]
         public string name { get; set; }
     }
 }
